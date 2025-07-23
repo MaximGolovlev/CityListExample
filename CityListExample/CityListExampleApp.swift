6b107aab46aae72fc27ec544d858e7afd71c1bb7
Maxim Golovlev <golovlev.maxim@gmail.com>
2025-07-23 17:26:33 +0300

    Initial Commit

---
+++ CityListExample/CityListExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityListExampleApp.swift
+//  CityListExample
+//
+//  Created by Максим on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct CityListExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
